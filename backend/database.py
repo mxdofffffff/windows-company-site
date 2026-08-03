29a5007c73a5798867fb705399437491209e96d9
mxdoff <madoffik@yandex.ru>
2026-08-03 22:54:38 +0500

    Setup FastAPI project and database configuration

---
+++ backend/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import create_async_engine,async_sessionmaker,AsyncSession
+from dotenv import load_dotenv
+from sqlalchemy.orm import DeclarativeBase
+import os
+
+load_dotenv(".env")
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_async_engine(DATABASE_URL,echo=True)
+
+SessionLocal = async_sessionmaker(bind=engine,class_=AsyncSession,expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
+
