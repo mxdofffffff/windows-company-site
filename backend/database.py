1c69d6f5f9af1f4c05036ff908e7a695cd294730
mxdoff <madoffik@yandex.ru>
2026-08-03 22:59:07 +0500

    feat: add requirements

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.asyncio import create_async_engine,async_sessionmaker,AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine,async_sessionmaker
 from dotenv import load_dotenv
 from sqlalchemy.orm import DeclarativeBase
 import os
@@ -7,7 +7,7 @@ load_dotenv(".env")
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_async_engine(DATABASE_URL,echo=True)
 
-SessionLocal = async_sessionmaker(bind=engine,class_=AsyncSession,expire_on_commit=False)
+SessionLocal = async_sessionmaker(bind=engine,expire_on_commit=False)
 
 class Base(DeclarativeBase):
     pass
