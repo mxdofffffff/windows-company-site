29a61aaa43ecd0ee700440a1c31769b68faceaf0
mxdoffffff <madoffik@yandex.ru>
2026-09-14 10:59:21 +0300

    feat: add CRUD for requests

--- backend/crud.py
+++ backend/crud.py
@@ -1,10 +1,10 @@
 from itertools import product
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from backend.models import Product
+from backend.models import Product,Request
 from sqlalchemy import select,func
 from sqlalchemy.orm import selectinload
-from backend.schemas import ProductCreate,ProductUpdate
+from backend.schemas import ProductCreate,ProductUpdate,RequestCreate
 
 async def get_products(db:AsyncSession,limit:int=10,skip:int=0,search:str|None = None,only_active:bool=True):
     query=select(Product).options(selectinload(Product.images))
@@ -51,3 +51,17 @@ async def deactivate_product(db:AsyncSession,product_id:int):
     product.is_active=False
     await db.commit()
     return product
+
+
+async def create_request(db:AsyncSession,request_data:RequestCreate):
+    new_request=RequestCreate(name=request_data.name,phone=request_data.phone,comment=request_data.comment,product_id=request_data.product_id)
+    db.add(new_request)
+    await db.commit()
+    await db.refresh(new_request)
+    return new_request
+
+
+async def get_requests(db:AsyncSession,limit:int=10,skip:int=0):
+    query=select(Request).order_by(Request.id.desc()).limit(limit).offset(skip)
+    result=await db.execute(query)
+    return result.scalars().all()
