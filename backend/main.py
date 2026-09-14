13f64bf6b40d23502948b3dd2a1cfc821287aac1
mxdoffffff <madoffik@yandex.ru>
2026-09-14 11:32:04 +0300

    feat: add request_router

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from backend.router import router
+from backend.routers.products import product_router
+from backend.routers.requests import request_router
 
 
 app = FastAPI(title="Window site")
-app.include_router(router)
+app.include_router(product_router)
+app.include_router(request_router)
